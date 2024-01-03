90144ee6cd0e935856581aaaceeeaf21262c1236
pythoncqnmd <3341219277@qq.com>
2024-01-03 21:01:52 +0800

    自动推送

---
+++ data/Untitled-1.py
@@ -0,0 +1,41 @@
+# %%
+print('这是什么玩意儿')
+
+# %%
+#以下是对变量的联系
+
+#数字类型
+money=100000
+print('初始资金'+str(money),type(money))
+money=money -19
+print('现有资金'+str(money),type(money))
+
+# %%
+#其他类型
+a=(1,2,3)
+print(type(a))
+b=[1,2,3]
+print(type(b))
+c={1,2,3}
+print(type(c))
+d={'姓名':'小明','性别':'男'}
+print(type(d))
+
+
+
+# %%
+#字符串
+#字符串是一种序列
+title='西北砍王'
+skill='不出玄晶'
+print(title+'一辈子'+skill)
+
+# %%
+#我也不知道这是什么
+'''
+作业
+作业
+作业
+'''
+
+
